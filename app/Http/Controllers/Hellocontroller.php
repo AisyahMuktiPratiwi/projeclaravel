163c0f4a0ad9ccca30debabe18b365164dcc7770
AisyahMuktiPratiwi <aisyahmuktipratiwi6725@gmail.com>
2020-09-08 14:45:51 +0700

    ini project pertama

---
+++ app/Http/Controllers/Hellocontroller.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Hellocontroller extends Controller
+{
+    //ini tampilan function index
+    public function index()
+    {
+        //echo "<h1>HELLO WORD!!!</h1>
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+ 
+//rute baru
+Route::get('HELLO', function () {
+    echo "<h1>HELLO WORD!!!</h1>";
+    echo "<h2> ini laravel Aisyah Mukti Pratiwi</h2>";
+});
